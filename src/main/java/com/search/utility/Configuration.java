5b7a14a342b12ac3aaed8016d519c6cb82bb1bc3
aditya-sachan <aditya.sachan.min16@itbhu.ac.in>
2021-10-13 10:48:26 +0530

    api key updated

--- src/main/java/com/search/utility/Configuration.java
+++ src/main/java/com/search/utility/Configuration.java
@@ -6,7 +6,7 @@ public class Configuration {
 	public static final String YOUTUBE_SEARCH_TYPE = "video";
 	public static final String YOUTUBE_SEARCH_FIELDS = "items(id/kind,id/videoId,id/channelId,snippet/title,snippet/description,snippet/publishedAt,snippet/channelTitle,snippet/thumbnails/default/url)";
 	public static final String YOUTUBE_API_APPLICATION = "google-youtube-api-search";
-	public static final String YOUTUBE_APIKEY_ENV = "AIzaSyB3qWQEGB_k4X0_V-oHfY7jBA9G0koLt_o";
+	public static final String YOUTUBE_APIKEY_ENV = "AIzaSyA3RWwfVVOP1BpM3_U8KdZP37JExXEhZ24";
 	public static final String YOUTUBE_PUBLISHED_AFTER_DATE = "2021-06-01T01:00:00.793+05:30";
 	public static final long NUMBER_OF_VIDEOS_RETURNED = 25;
 	public static int API_KEY_NUMBER = 1;
