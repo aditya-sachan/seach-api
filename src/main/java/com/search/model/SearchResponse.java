7751d4102eb0c27d7290e154e94233be5c59b964
aditya-sachan <aditya.sachan.min16@itbhu.ac.in>
2021-10-13 10:33:24 +0530

    model created, youtube api properties added and in memory db created

--- src/main/java/com/search/model/SearchResponse.java
+++ src/main/java/com/search/model/SearchResponse.java
@@ -1,5 +1,21 @@
 package com.search.model;
 
-public class SearchResponse {
+import com.google.api.client.util.DateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+public class SearchResponse {
+	private String videoId;
+    private String url;
+    private String title;
+    private String thumbnailDefaultURL;
+    private String description;
+    private String channelId;
+    private String channelTitle;
+    private DateTime publishedAt;
 }
--- src/main/java/com/search/repository/InMemoryDB.java
+++ src/main/java/com/search/repository/InMemoryDB.java
@@ -1,5 +1,22 @@
 package com.search.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.search.model.SearchResponse;
+
 public class InMemoryDB {
+	private static List<SearchResponse> videoSearchDataStore = new ArrayList<>();
+
+	public static List<SearchResponse> getVideoSearchDataStore() {
+		return videoSearchDataStore;
+	}
+
+	public static void addVideoSearchDataStore(List<SearchResponse> videoSearchDataStore) {
+		InMemoryDB.videoSearchDataStore.addAll(videoSearchDataStore);
+	}
 	
+	public static void addVideoSearchDataStore(SearchResponse videoSearchDataStore) {
+		InMemoryDB.videoSearchDataStore.add(videoSearchDataStore);
+	}
 }
